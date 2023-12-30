aec54f00ceb112105deb8568987641afd117041b
R.S.W <ennorswu@gmail.com>
2023-12-31 02:07:40 +0800

    iterating through and print out 2-dimensional map

--- scripts.js
+++ scripts.js
@@ -7,22 +7,45 @@
 
     // map
     // 0: road      1: wall      2: roads with points
-    const map = [1, 1, 1, 1, 1, 1, 1, 1];
-    pos_x = 50;
-    pos_y = 50;
-    scale_x = 1000;
+    const map = [
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 0, 0, 0, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],                    
+                ];
+    initial_x = 50;
+    initial_y = 50;
+    pos_x = initial_x;
+    pos_y = initial_y;
+    scale_x = 30;
     scale_y = 12;
     ctx.lineWidth = 3;
-    map.forEach(item => {
-        if(item == 1) {
-            ctx.fillStyle = 'black';
-            ctx.strokeStyle = '#393ff1';
-            ctx.fillRect(pos_x, pos_y, scale_x, scale_y);
-            ctx.strokeRect(pos_x, pos_y, scale_x, scale_y);
-            pos_x += scale_x;
-        };
-    });
+    for(let i = 0; i < map.length; i++) 
+    {
+        let row = map[i];
 
-    // draw a rectangle with fill and stroke
+        for(let j = 0; j < map[i].length; j++) 
+        {
+            // printing each grid
+            if(row[j] == 0) 
+            {
+                ctx.fillStyle = '#fff';
+                ctx.fillRect(pos_x, pos_y, scale_x, scale_y);
+                pos_x += scale_x;
+            }    
+            if(row[j] == 1) 
+            {
+                ctx.fillStyle = 'black';
+                ctx.strokeStyle = '#393ff1';
+                ctx.fillRect(pos_x, pos_y, scale_x, scale_y);
+                ctx.strokeRect(pos_x, pos_y, scale_x, scale_y);
+                pos_x += scale_x;
+            }    
+        }
+        // a row has ended, reset the starting point
+        pos_x = initial_x;
+        pos_y = initial_y + (scale_y * i);
+    }
+
 
 })();
