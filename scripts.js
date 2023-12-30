9ae617abf5bd302c669f94d1696a22f5f913ae82
R.S.W <ennorswu@gmail.com>
2023-12-31 01:39:48 +0800

    adjust walls + strokes width and total scale

--- scripts.js
+++ scripts.js
@@ -4,19 +4,21 @@
     // get the 2D context, and then I am able to draw somnething on it 
     let ctx = canvas.getContext('2d');
 
-    // set fill and stroke styles
-    ctx.fillStyle = '#F0DB4F';
 
     // map
     // 0: road      1: wall      2: roads with points
     const map = [1, 1, 1, 1, 1, 1, 1, 1];
     pos_x = 50;
     pos_y = 50;
-    scale_x = 20000;
+    scale_x = 1000;
+    scale_y = 12;
+    ctx.lineWidth = 3;
     map.forEach(item => {
         if(item == 1) {
-            ctx.fillStyle = '#393ff1';
-            ctx.fillRect(pos_x, pos_y, scale_x, 2);
+            ctx.fillStyle = 'black';
+            ctx.strokeStyle = '#393ff1';
+            ctx.fillRect(pos_x, pos_y, scale_x, scale_y);
+            ctx.strokeRect(pos_x, pos_y, scale_x, scale_y);
             pos_x += scale_x;
         };
     });
