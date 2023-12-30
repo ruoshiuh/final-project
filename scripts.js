cc16e306da9feaf50f531873fc67837e7da79f79
R.S.W <ennorswu@gmail.com>
2023-12-31 01:12:25 +0800

    check for map and iteration

--- scripts.js
+++ scripts.js
@@ -6,10 +6,14 @@
 
     // set fill and stroke styles
     ctx.fillStyle = '#F0DB4F';
-    ctx.strokeStyle = 'red';
+
+    // map
+    const map = [1, 0, 1, 0, 1, 0, 1, 0];
+    map.forEach(item => {
+        console.log(`${item}`)
+    });
 
     // draw a rectangle with fill and stroke
     ctx.fillRect(50, 50, 500, 500);
-    ctx.strokeRect(50, 50, 500, 500);
 
 })();
