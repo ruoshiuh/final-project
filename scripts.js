ca9c128910e31c84c799264bd0107256b0ad6207
R.S.W <ennorswu@gmail.com>
2023-12-31 09:11:21 +0800

    design and expand the map

--- scripts.js
+++ scripts.js
@@ -8,24 +8,26 @@
     // map
     // 0: road      1: wall      2: roads with points
     const map = [
-                    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1],
+                    [1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],                                    
                 ];
 
-    block = 30;
+    block = 40;
     for(let i = 0; i < map.length; i++) 
     {
         let row = map[i];
-
         for(let j = 0; j < map[i].length; j++) 
         {
-            // printing each grid
+            // painting out the road
             if(row[j] == 0) 
             {
-                ctx.fillStyle = '#fff';
+                ctx.fillStyle = 'black';
                 ctx.fillRect(block * j, block * i, block, block);
             }    
+            // painting out the wall
             if(row[j] == 1) 
             {
                 ctx.fillStyle = '#393ff1';
