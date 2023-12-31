ee08478ff9962f66ff4bfb7d2b131fb9fe76ffeb
R.S.W <ennorswu@gmail.com>
2023-12-31 11:37:04 +0800

    start to paint the inner boxs of walls

--- scripts.js
+++ scripts.js
@@ -15,26 +15,46 @@
                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],                                    
                 ];
 
-    block = 40;
+    let block = 40;
+    let padding = block * 0.2;
+
+    // default walls and roads
     for(let i = 0; i < map.length; i++) 
     {
         let row = map[i];
         for(let j = 0; j < map[i].length; j++) 
         {
-            // painting out the road
+            // the roads
             if(row[j] == 0) 
             {
                 ctx.fillStyle = 'black';
                 ctx.fillRect(block * j, block * i, block, block);
             }    
-            // painting out the wall
+            // the walls
             if(row[j] == 1) 
             {
                 ctx.fillStyle = '#393ff1';
                 ctx.fillRect(block * j, block * i, block, block);
-            }    
+            } 
         }
     }
+    // painting the inner box for walls
+    for(let i = 0; i < map.length; i++) 
+    {
+        let row = map[i];
+        for(let j = 0; j < map[i].length; j++) 
+        {
+            pos_x = block * j;
+            pos_y = block * i;
+            // 1. adjacent left and right
+            if(row[j] == 1 && row[j + 1] == 1) 
+            {
+                ctx.fillStyle = 'black';
+                ctx.fillRect(pos_x + padding, pos_y + padding, block * 2 - padding * 2, block - padding * 2);
+            } 
+        }
+    }
+   
 
 
 })();
