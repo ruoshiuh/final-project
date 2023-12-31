1120418cbb1c535c55d8b8690c724de6a0b6d469
R.S.W <ennorswu@gmail.com>
2023-12-31 08:28:12 +0800

    find out what went wrong with map iteration and refine

--- scripts.js
+++ scripts.js
@@ -8,17 +8,15 @@
     // map
     // 0: road      1: wall      2: roads with points
     const map = [
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 0, 0, 0, 0, 0, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1],                    
+                    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                    
                 ];
     initial_x = 50;
     initial_y = 50;
     pos_x = initial_x;
     pos_y = initial_y;
-    block_x = 30;
-    block_y = 12;
+    block = 30;
     ctx.lineWidth = 3;
     for(let i = 0; i < map.length; i++) 
     {
@@ -26,25 +24,26 @@
 
         for(let j = 0; j < map[i].length; j++) 
         {
+            console.log(i, j, pos_x, pos_y, map[i][j]);
             // printing each grid
             if(row[j] == 0) 
             {
                 ctx.fillStyle = '#fff';
-                ctx.fillRect(pos_x, pos_y, block_x, block_y);
-                pos_x += block_x;
+                ctx.fillRect(pos_x, pos_y, block, block);
+                pos_x += block;
             }    
             if(row[j] == 1) 
             {
                 ctx.fillStyle = 'black';
                 ctx.strokeStyle = '#393ff1';
-                ctx.fillRect(pos_x, pos_y, block_x, block_y);
-                ctx.strokeRect(pos_x, pos_y, block_x, block_y);
-                pos_x += block_x;
+                ctx.fillRect(pos_x, pos_y, block, block);
+                ctx.strokeRect(pos_x, pos_y, block, block);
+                pos_x += block;
             }    
         }
         // a row has ended, reset the starting point
         pos_x = initial_x;
-        pos_y = initial_y + (block_y * i);
+        pos_y = initial_y + block * (i + 1);
     }
 
 
