ed8833da9b73352e20f3b7a511a647f6ac277faf
R.S.W <ennorswu@gmail.com>
2024-01-01 07:57:41 +0800

    inner block display error fixed & expand the map

--- scripts.js
+++ scripts.js
@@ -15,8 +15,9 @@
                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                     [1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1],
                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0]
                 ];
 
     let block = 40;
@@ -48,6 +49,7 @@
         let row = map[i];
         for(let j = 0; j < map[i].length; j++) 
         {
+            console.log(i, j, map[i][j])
             pos_x = block * j;
             pos_y = block * i;
             // 1. just one block
@@ -57,20 +59,20 @@
                 ctx.fillRect(pos_x + padding, pos_y + padding, block - padding * 2, block - padding * 2);
             }
             // 2. adjacent left and right
-            if(row[j] == 1 && row[j + 1] == 1) 
+            if(j < (row.length - 1) && row[j] == 1 && row[j + 1] == 1) 
             {
                 ctx.fillStyle = 'black';
                 ctx.fillRect(pos_x + padding, pos_y + padding, block * 2 - padding * 2, block - padding * 2);
             } 
             // 3. adjacent top and botttom
-            if(row[j] == 1 && map[i + 1][j] == 1)
+            if(i < (map.length - 1) && row[j] == 1 && map[i + 1][j] == 1)
             {
                 ctx.fillStyle = 'black';
                 ctx.fillRect(pos_x + padding, pos_y + padding, block  - padding * 2, block * 2 - padding * 2);
 
             }
             // 4. all four adjacent block: top bottom left right 
-            if (row[j] == 1 && row[j + 1] == 1 && map[i + 1][j] == 1 && map[i + 1][j + 1] == 1)
+            if (j < (row.length - 1) && i < (map.length - 1) && row[j] == 1 && row[j + 1] == 1 && map[i + 1][j] == 1 && map[i + 1][j + 1] == 1)
             {
                 ctx.fillStyle = 'black';
                 ctx.fillRect(pos_x + padding, pos_y + padding, block * 2 - padding * 2, block * 2 - padding * 2);
